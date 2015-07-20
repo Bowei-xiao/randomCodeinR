b5deba4088e50217f64cf6ec085f6b917d80452f
work.adam.xiao@gmail.com <bowei@ches-2048-1nm.sickkids.ca>
2015-07-20 11:19:39 -0400

    Calculate Egyptian Fraction for any rational function <1

---
+++ egyptFraction.R
@@ -0,0 +1,24 @@
+# Function to calculate the Egyptian Fraction for any rational fraction <1
+gcd = function(x,y) {
+# Helper Function get the greatest common divisor, assuming x>=y  
+  r = x%%y;
+  return(ifelse(r, gcd(y, r), y))
+}
+
+egyFrac = function(a,b,lane=NULL){
+# Main function. a is the numerator, and b is the denominator
+  if (a==1){
+    lane = paste0(lane,'1/',b)
+    return(lane)
+  } else{
+    a_new = (-b %% a)
+    b_new = b*(as.integer(b/a)+1)
+    lane_new = paste0(lane,'1/',(as.integer(b/a)+1),'+')
+    cd = gcd(b_new,a_new)
+    if (cd != 1){
+      b_new =b_new / cd
+      a_new =a_new / cd
+      }
+    egyFrac(a=a_new,b=b_new,lane=lane_new)
+  }
+}
